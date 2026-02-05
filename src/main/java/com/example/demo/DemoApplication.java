2f4afa85d29b30f9ef2f4c039f3d9b7e06b4d724
JASSNexocorp <JSAHONERO@farmacorp.com>
2026-02-05 07:18:53 -0400

    testing

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -4,6 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 
 @SpringBootApplication
 public class DemoApplication {
@@ -14,13 +16,23 @@ public class DemoApplication {
 
 @RestController
 class HelloController {
+
+    @Autowired
+    private Enviroment environment;
+
     @GetMapping("/")
     public String hello() {
-        return "Hello CI/CD World!";
+        return "Hello CI/CD World! - Entrega Final";
     }
 
     @GetMapping("/health")
     public String health() {
-        return "Health check OK";
+        return "Health check passed!";
+    }
+
+    @GetMapping("/instance")
+    public String instance() {
+        String port = environment.getProperty("local.server.port");
+        return "Instancia corriendo en el puerto: " + port;
     }
 }
