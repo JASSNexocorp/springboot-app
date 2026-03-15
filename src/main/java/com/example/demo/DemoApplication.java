49cb47d828394801e01b6441f69ba39d8f1700ec
JASSNexocorp <JSAHONERO@farmacorp.com>
2026-03-15 12:59:41 -0400

    cambios

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -38,6 +38,6 @@ class HelloController {
 
     @GetMapping("/version")
     public String version() {
-        return "Versión Servicio CI/CD: 01.15.03.2026";
+        return "Versión Servicio CI/CD: 02.15.03.2026";
     }
 }
